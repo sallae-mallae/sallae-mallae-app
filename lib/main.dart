8091db2dfabe184007feff28a630fbe3061776a7
b0e2 <beenjeong02@gmail.com>
2026-06-07 15:36:04 +0900

    chore: add riverpod provider scope

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'app/app.dart';
 
 void main() {
-  runApp(const SallaeMallaeApp());
+  runApp(const ProviderScope(child: SallaeMallaeApp()));
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,9 +1,10 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:sallae_mallae_app/app/app.dart';
 
 void main() {
   testWidgets('App renders', (WidgetTester tester) async {
-    await tester.pumpWidget(const SallaeMallaeApp());
+    await tester.pumpWidget(const ProviderScope(child: SallaeMallaeApp()));
 
     expect(find.byType(SallaeMallaeApp), findsOneWidget);
   });
