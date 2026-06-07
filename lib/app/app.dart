7eb674d1c6b5ae55f129fb3a91729cfa9c66fa37
b0e2 <beenjeong02@gmail.com>
2026-06-07 15:30:26 +0900

    chore: organize app entrypoint

---
+++ lib/app/app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SallaeMallaeApp extends StatelessWidget {
+  const SallaeMallaeApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Sallae Mallae',
+      debugShowCheckedModeBanner: false,
+      home: const Placeholder(),
+    );
+  }
+}
